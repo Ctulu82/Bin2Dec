fd402bb28db3d357972ea4c8a8321cb3f9bad95b
TaeHyun Kim <taehyun.kim1982@gmail.com>
2021-02-09 14:38:42 +0900

    Binary to decimal function added.

--- Bin2Dec/Transformer.swift
+++ Bin2Dec/Transformer.swift
@@ -15,4 +15,17 @@ class Transformer {
             return decToBin(number / 2) + "1"
         }
     }
+    
+    func binToDec(_ binary: String) -> Int? {
+        var result: Int = 0
+
+        for digit in binary {
+            switch(digit) {
+                case "0": result = result * 2
+                case "1": result = result * 2 + 1
+                default: return nil
+            }
+        }
+        return result
+    }
 }
--- Bin2Dec/main.swift
+++ Bin2Dec/main.swift
@@ -5,3 +5,4 @@ let transformer = Transformer()
 
 print(transformer.decToBin(30))
 
+print(transformer.binToDec("11110")!)
