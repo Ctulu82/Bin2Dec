1bb078f34a0ea5cb627a79bffbfabfaf540e0e8f
TaeHyun Kim <taehyun.kim1982@gmail.com>
2021-02-09 14:40:16 +0900

    Add the BSD library function 'strtoul()'.

--- Bin2Dec/main.swift
+++ Bin2Dec/main.swift
@@ -6,3 +6,7 @@ let transformer = Transformer()
 print(transformer.decToBin(30))
 
 print(transformer.binToDec("11110")!)
+
+let binary = "11001"
+let number = strtoul(binary, nil, 2)
+print(number) // Output: 25
